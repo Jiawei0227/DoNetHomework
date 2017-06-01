e13cc8e2c5f7f298712cda65aa9e408125fd9409
WJerry0227 <wangjiawei0227@163.com>
2017-06-01 19:52:57 +0800

    Finish of Homework4

---
+++ Homework4/Homework4/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Homework4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(299, 75);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(807, 66);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(299, 177);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(807, 70);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(299, 288);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(807, 70);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(104, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 41);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "进度条1：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(104, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 41);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "进度条2：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(104, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 41);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "进度条3：";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(832, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 62);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(982, 500);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 62);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "暂停";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(1136, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 37);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "0%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(1136, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 37);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0%";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(1136, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 37);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "0%";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(299, 405);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(807, 30);
+            this.hScrollBar1.TabIndex = 11;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(104, 417);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(154, 41);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "进度比例:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.ForeColor = System.Drawing.Color.Blue;
+            this.label8.Location = new System.Drawing.Point(1143, 405);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 37);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "10倍";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1268, 619);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "多线程测试作业";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Homework4/Homework4/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Homework4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            hScrollBar1.Value = 100;
+        }
+
+        int beishu = 10;
+        Thread t1,t2,t3;
+        TimeKeeper timekeeper;
+        MyProgressBar myProgressBar1, myProgressBar2, myProgressBar3;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //时间线程
+            timekeeper = new TimeKeeper();
+            Thread timeT = new Thread(new ThreadStart(timekeeper.runMethod));
+            timeT.Start();
+
+            //第一个进度条线程
+            myProgressBar1 = new MyProgressBar(1,timekeeper);
+            myProgressBar1.setProgressBar += setProgress1Value;
+            t1 = new Thread(new ThreadStart(myProgressBar1.runMethod));
+            t1.Start();
+
+            //第二个进度条线程
+            myProgressBar2 = new MyProgressBar(2,timekeeper);
+            myProgressBar2.setProgressBar += setProgress2Value;
+            t2 = new Thread(new ThreadStart(myProgressBar2.runMethod));
+            t2.Start();
+
+            //第三个进度条线程
+            myProgressBar3 = new MyProgressBar(3,timekeeper);
+            myProgressBar3.setProgressBar += setProgress3Value;
+            t3 = new Thread(new ThreadStart(myProgressBar3.runMethod));
+            t3.Start();
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+           label8.Text = ((hScrollBar1.Value+10)/10).ToString() + "倍";
+            beishu = (hScrollBar1.Value+10) / 10;
+
+        }
+
+        private void setProgress1Value(int value,int number)
+        {
+            if (this.progressBar1.InvokeRequired)
+            {
+                MyProgressBar m = new MyProgressBar(number);
+                m.setProgressBar += setProgress1Value;
+                this.Invoke(m.setProgressBar, new Object[] { value, number });
+            }
+            else
+            {
+                if ((value / 100) > 0 && (value % 100) == 0)
+                {
+                    this.progressBar1.Value = 100;
+                    this.label4.Text =  "100%";
+                }
+                else
+                {
+                    this.progressBar1.Value = value % 100;
+                    this.label4.Text = (value % 100).ToString() + "%";
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timekeeper.isStop = true;
+            myProgressBar1.isStop = true;
+            myProgressBar2.isStop = true;
+            myProgressBar3.isStop = true;
+        }
+
+        private void setProgress2Value(int value, int number)
+        {
+            if (this.progressBar2.InvokeRequired)
+            {
+                MyProgressBar m = new MyProgressBar(number);
+                m.setProgressBar += setProgress2Value;
+                this.Invoke(m.setProgressBar, new Object[] { value, number });
+            }
+            else
+            {
+                if ((value / beishu) > 0 && ((value / beishu) % 100) == 0)
+                {
+                    this.progressBar2.Value = 100;
+                    this.label5.Text = "100%";
+                }
+                else
+                {
+                    this.progressBar2.Value = (value / beishu) % 100;
+                    this.label5.Text = ((value / beishu) % 100).ToString() + "%";
+                }
+            }
+        }
+
+        private void setProgress3Value(int value, int number)
+        {
+            if (this.progressBar3.InvokeRequired)
+            {
+                MyProgressBar m = new MyProgressBar(number);
+                m.setProgressBar += setProgress3Value;
+                this.Invoke(m.setProgressBar, new Object[] { value, number });
+            }
+            else
+            {
+                if (value / (beishu * beishu) == 100)
+                    this.button2_Click(new object(),new EventArgs());
+                this.progressBar3.Value = value / (beishu*beishu);
+                this.label6.Text = (value /(beishu*beishu)).ToString()+"%";
+            }
+        }
+         
+    }
+
+    public class TimeKeeper
+    {
+        int time;
+        public bool isStop = false;
+        public void runMethod()
+        {
+            while (!isStop)
+            {
+                Thread.Sleep(100);
+                time+=5;
+            }
+        }
+
+        public int getTime()
+        {
+           return time;
+         
+        }
+    }
+
+
+    public class MyProgressBar
+    {
+        //声明一个委托
+        public delegate void setProgressBarValue(int value,int number);
+        public setProgressBarValue setProgressBar;
+        int number;
+        public bool isStop = false;
+        TimeKeeper t;
+
+        public MyProgressBar(int number)
+        {
+            this.number = number;
+        }
+
+        public MyProgressBar(int number,TimeKeeper t)
+        {
+            this.number = number;
+            this.t = t;
+        }
+        public void runMethod()
+        {
+            while (!isStop)
+            {
+                Thread.Sleep(50);
+                lock (t)
+                {
+                    setProgressBar(t.getTime(), number);
+                }
+            }
+                
+            
+        }
+    }
+}
